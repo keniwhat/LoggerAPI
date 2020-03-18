9d237088d2077b6803746006b4479d74e24ae74b
Ken Carroll <ken@mangofever.com>
2020-03-18 19:01:11 +0000

    Use local copy with xcode project

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
     dependencies: [
         // Note: Syntax to allow compatibility with both Swift 4 and Swift 5 projects.
         // See: https://github.com/apple/swift-log#help-i-need-swift-4
-        .package(url: "https://github.com/apple/swift-log.git", Version("0.0.0") ..< Version("2.0.0")),
+        .package(url: "https://github.com/keniwhat/swift-log.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
